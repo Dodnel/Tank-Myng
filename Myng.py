7dc8b620c202d82c6f6f554df62aa5f65c0ab104
Ander <anderkonsap@gmail.com>
2025-03-26 11:48:28 +0200

    Muudatused Myng.py-s

--- Myng.py
+++ Myng.py
@@ -2,10 +2,16 @@ import pygame
 
 class Myng:
     def __init__(self):
+        pygame.init()
+        self.ekraan = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
         pass
 
     def events(self):
-        pass
+        for event in pygame.event.get():
+            pass
+
 
     def run(self):
-        pass
+        while True:
+            self.clock.tick(60)
