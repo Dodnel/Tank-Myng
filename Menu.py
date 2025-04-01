0ffa7cff8a8ed9de3c5afa71242a5cc4e29f70c8
tonyt <tony.tuisk@opilased.nrg.edu.ee>
2025-04-01 14:01:29 +0300

    muudatus 1

--- Menu.py
+++ Menu.py
@@ -4,3 +4,11 @@ class Menu:
     def __init__(self):
         self.aken = tk.Tk()
         self.aken.title("Tank myng")
+        self.aken.geometry("500x500")
+
+    def looVidinad(self):
+        pass
+
+    def main(self):
+        self.aken.mainloop()
+
