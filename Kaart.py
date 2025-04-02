760e1d88910c850468bdaceacf1a1c9d9319619f
Ander <anderkonsap@gmail.com>
2025-04-02 11:41:41 +0300

    Vyiksed muudatused ja ideed kaardi loomisel

--- Kaart.py
+++ Kaart.py
@@ -10,10 +10,29 @@ class Kaart:
 
 
     def drawMap(self,ekraan):
+        """
+        Tuleb otsustada kui suur üks tile on
+        idee,
+        Joonistamiseks, paneb igale tileile tyhti mis näitab mis ilmakaartel sellel tileil on sein
+        N (north) tilei põhja suunas on sein
+        S (south) tilei lõuna sunuas on sein
+        W (west) sa saad aru
+        E (east) sa saad aru
+        Neid saab stackida ka et NSW on lubatud sisend yhe tile jaoks
+        ning siis lõppu võib lisada veel mingi tähe mis tähitab tilei pilti kui me tahame keerulisemaid textuure
+        nt: 3x3 map [["NW", "N", "NE"],
+                     ["W", "", "E"],
+                     ["SW", "S", "SE"]]
+
+        :param ekraan:
+        :return:
+        """
+
 
         pass
 
     def genereeriMap(self):
+        #idee
         kaart = numpy.array([1, 2, 3, 4, 5])
 
         return kaart
