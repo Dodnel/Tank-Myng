257e9d0bb285a66768b7db28d1325e47d994999b
Ander (Dodnel) <anderkonsap@gmail.com>
2025-04-06 15:55:00 +0300

    Tangis plaani lisamine

--- Tank.py
+++ Tank.py
@@ -4,10 +4,27 @@ Igastahes collisioneid vaadates, paneme talle peale ühe nähtamatu recti, mis k
 Kui ei pane seda recti peame kontrollima kas ta kontrollib kõik seintega mapi peal mis ei ole praktiline
 
 """
+import pygame
 
-class Tank:
-    def __init__(self):
-        pass
+#see on varastatud, ma vist teen yppimise pyhimyttel ise mingi hetk
+class Tank(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, vyrv):
+        pygame.sprite.Sprite.__init__(self)
+        self.angle = 0
+        self.original_image = pygame.Surface([w, h], pygame.SRCALPHA)
+        self.original_image.fill(vyrv)
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(x, y))
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def keera(self, suund):
+        if suund == 1:
+            self.angle += 1
+        elif suund == -1:
+            self.angle -= 1
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def joonistaTank(self):
-        pass
+        return ""
--- testMyng.py
+++ testMyng.py
@@ -1,30 +1,65 @@
+from winreg import KEY_WRITE
+
 import pygame, sys
 from Kaart import Kaart
 from Kuul import Kuul
-
+from pygame.locals import *
+from Tank import Tank
 resolutsioon = "900x500"
 
 laius, kyrgus = map(int,resolutsioon.split("x"))
 
 pygame.init()
-
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((laius,kyrgus))
 
 kaart = Kaart(resolutsioon=resolutsioon,tileSuurus=50)
 #kuul = Kuul()
+tank = Tank(50, 50, 20, 20, (128, 0, 255))
 
+liikuvadAsjad = pygame.sprite.Group(tank)
 
 kaart.randomizedKruskalAlgoritm()
 
 seinad = kaart.drawMap()
 print(seinad)
-
+vajutus = 0
+nuppAll = False
 while True:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_d]:
+                vajutus = 1
+            elif keys[pygame.K_a]:
+                vajutus = -1
+            else:
+                pass
+            nuppAll = True
+
+        if event.type == pygame.KEYUP:
+            keys = pygame.key.get_pressed()
+            if not keys[pygame.K_d]:
+                vajutus = 0
+                nuppAll = False
+            elif not keys[pygame.K_a]:
+                vajutus = 0
+                nuppAll = False
+            else:
+                pass
+
+
+    if nuppAll:
+        tank.keera(vajutus)
 
     screen.fill("white")
     for sein in seinad:
         pygame.draw.rect(screen,"black", sein)
+    liikuvadAsjad.update()
+    liikuvadAsjad.draw(screen)
+
     pygame.display.flip()
