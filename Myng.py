020e513409822535c74446b99ce48cac0025cda7
Ander <anderkonsap@gmail.com>
2025-05-20 14:04:16 +0300

    Mynguloogika

--- Myng.py
+++ Myng.py
@@ -1,5 +1,8 @@
 import pygame
-import Kaart,Menu,Kuul,Tank,liikumine
+from Kaart import Kaart
+from Kuul import Kuul
+from Tank import Tank
+from liikumine import Liikumine
 import sys
 #s
 class Myng:
@@ -7,38 +10,46 @@ class Myng:
         pygame.init()
         self.ekraan = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
+        self.kaart = Kaart(resolutsioon, tileSuurus)
+        self.liikumisProfiilid = tankideLiikumisProfiilid
         self.seinad = []
-        self.kaart = Kaart.Kaart(resolutsioon, tileSuurus)
-        self.tankideLiikumine = tankideLiikumisProfiilid
-
 
     def looKaart(self):
         self.kaart.lammutaKaart()
         self.kaart.randomizedKruskalAlgoritm()
-        self.seinad = self.kaart.drawMap()
-        pass
+        seinad = self.kaart.drawMap()
+
+        return seinad
+
 
     def looTankid(self):
+        tankid = []
         tekkeKohad = self.kaart.leiaTankideleTekkeKohad(len(self.tankideLiikumine))
+        for koht in tekkeKohad:
+            tankid.append(Tank(koht[0], koht[1], 20, 30))
 
-        pass
+        return tankid
 
 
     def nullindaSkoor(self):
         pass
 
-
-
-
     def events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        vajutused = pygame.key.get_pressed()
+        return [pygame.key.name(k) for k in range(len(vajutused)) if vajutused[k]]
+
 
     def collisionCheck(self):
         pass
 
     def run(self):
-        nullindaSkoor()
-        looKaart()
-        looTankid()
+        self.seinad = self.looKaart()
+        tankid = self.looTankid()
+        liikumine = Liikumine(tankid,self.liikumisProfiilid)
+        while True:
+            clock.tick(60)
 
         pass
